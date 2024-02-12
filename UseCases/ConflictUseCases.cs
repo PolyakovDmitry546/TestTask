e41875cf2d3abcf343c79f6d9e2ba348a364ac0d
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-02-12 22:42:08 +0500

    Добавить сохранение списка конфликтов в UseCases

---
+++ UseCases/ConflictUseCases.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure;
+using Infrastructure.DTOs;
+
+
+namespace UseCases
+{
+    public class ConflictUseCases
+    {
+        private readonly IConflictRepository repository;
+        public ConflictUseCases(IConflictRepository repository) 
+        {
+            this.repository = repository;
+        }
+
+        public void SaveConflicts(List<Conflict> conflicts) 
+        {
+            foreach (var conflict in conflicts)
+            {
+                repository.Insert(conflict);
+            }
+            repository.Save();
+        }
+    }
+}
