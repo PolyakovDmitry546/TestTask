e6a6f59b887da2b92def9f55afc9b8cb1f616a4c
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-02-12 22:51:05 +0500

    Добавить считывание информации о приборах, поиск и сохранение конфликтов в json формате

--- Presentation/ConsoleApp/Program.cs
+++ Presentation/ConsoleApp/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using UseCases;
+using Infrastructure;
+
+
+JsonDeviceInfoRepository deviceInfoRepository = new("Devices.json");
+DeviceInfoUseCases deviceInfoUseCases = new(deviceInfoRepository);
+var conflicts = deviceInfoUseCases.FindConflicts();
+JsonConflictRepository conflictRepository = new("Conflicts.json");
+ConflictUseCases conflictUseCases = new(conflictRepository);
+conflictUseCases.SaveConflicts(conflicts);
