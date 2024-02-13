226e04a91af66529911c58c418996f25efbc5d48
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-02-13 16:08:09 +0500

    Добавить возможность передачи путей к файлу приборов и конфликтов через аргументы командной строки

--- Presentation/ConsoleApp/Program.cs
+++ Presentation/ConsoleApp/Program.cs
@@ -2,9 +2,15 @@
 using Infrastructure;
 
 
-JsonDeviceInfoRepository deviceInfoRepository = new("Devices.json");
+string devicesPath = "Devices.json";
+string conflictPath = "Conflicts.json";
+
+if (args.Length > 0) devicesPath = args[0];
+if (args.Length > 1) conflictPath = args[1];
+
+JsonDeviceInfoRepository deviceInfoRepository = new(devicesPath);
 DeviceInfoUseCases deviceInfoUseCases = new(deviceInfoRepository);
 var conflicts = deviceInfoUseCases.FindConflicts();
-JsonConflictRepository conflictRepository = new("Conflicts.json");
+JsonConflictRepository conflictRepository = new(conflictPath);
 ConflictUseCases conflictUseCases = new(conflictRepository);
 conflictUseCases.SaveConflicts(conflicts);
